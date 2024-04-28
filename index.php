ddd413ac9de3a2add9db2878618969f7200342ff
touzouz <imad.touzouz1@gmail.com>
2024-04-28 14:59:42 +0100

    add new multi file

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="style/css/index.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="file/logo.png" type="image/x-icon">
     <title>الجيل المبرمج العربي</title>
 </head>
 <body>
