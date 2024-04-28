10644b28e92051ad1667884363dea0bf1ab7340e
touzouz <imad.touzouz1@gmail.com>
2024-04-28 14:27:13 +0100

    add index file index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>الجيل المبرمج العربي</title>
+</head>
+<body>
+    <h1>الجيل المبرمج العربي</h1>
+</body>
+</html>
