1bad3bdcffbd46d9121bd80048198c48cd4ed5f5
ElBaron23 <imad.touzouz1@gmail.com>
2024-07-12 20:47:41 +0200

    edit profile page

--- profile.php
+++ profile.php
@@ -1,19 +1,34 @@
-<?php
-    session_start();
-    include 'inc/if_not_regi.php';
-    include 'inc/navbar.php';
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="style/css/home.css">
+    <link rel="stylesheet" href="style/css/profile.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>الملف الشخصي</title>
 </head>
 <body dir="rtl">
+<?php
+    session_start();
+    include 'inc/if_not_regi.php';
+    include 'inc/navbar.php';
+
+    $firstname = $_SESSION['data']->firstname;
+    $lastname  = $_SESSION['data']->lastname;
+    $email = $_SESSION['data']->email;
+?>
 <div class="container">
-    
+    <div class="user_profile">
+       
+        <div class="name">
+        <h1><?php echo  $firstname ." ". $lastname ?></h1>
+        <h4><?php  echo $email ?></h4>
+        <button type="submit" id="edit_profile">تعديل الملف الشخصي</button>
+        </div>
+        <div id="photo_profile">
+        </div>
+      
+    </div>
 </div>    
 
 </body>
