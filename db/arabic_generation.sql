50c7567c8e54ecca129326e5ce044728f5df061e
ElBaron23 <imad.touzouz1@gmail.com>
2024-07-17 15:55:38 +0200

    اضافة حقلين في جدول قاعدة البيانات

--- db/arabic_generation.sql
+++ db/arabic_generation.sql
@@ -32,6 +32,8 @@ CREATE TABLE `admin` (
   `user` varchar(25) NOT NULL,
   `password` varchar(50) NOT NULL,
   `phone` varchar(50) NOT NULL,
+  `gender` varchar(50) NOT NULL,
+  `birth` varchar(50) NOT NULL
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
