c2f8987f20a82c64f0ddf458a6e6ad5eaffcb944
ElBaron23 <imad.touzouz1@gmail.com>
2024-07-17 16:27:12 +0200

    تعديلات على صفحة الخطا

--- other/error.php
+++ other/error.php
@@ -11,23 +11,48 @@ include '../inc/init.php';
     <title>صفحة غير موجودة حاليا</title>
 </head>
 <body dir="rtl">
-    <?php
-    include $cnx;
-    ?>
-    <header>
-        <?php
-include $navIndex
-        ?>
-    </header>
- 
-    <img id="img360" src="images/icon-white.png">
+      <style>
+      body { 
+        background-color: #191919; 
+        text-align: center; 
+        padding: 150px; }
+      h1 { 
+        font-size: 50px; }
+      body { 
+        font: 20px Helvetica, sans-serif; color: #fff; 
+      }
+      .links{
+        
+      }
+      #img360 {
+        border-radius: 100px;
+        margin-left: 0px;
+        -webkit-animation:spin 4s linear infinite;
+        -moz-animation:spin 4s linear infinite;
+        animation:spin 4s linear infinite;
+      }
+      @-moz-keyframes spin {
+         100% { 
+            -moz-transform: rotate(360deg);
+         }
+         }
+      @-webkit-keyframes spin { 
+        100% {
+            -webkit-transform: rotate(360deg); 
+        } 
+    }
+      @keyframes spin { 
+        
+        100% { 
+            -webkit-transform: rotate(360deg);
+             transform:rotate(360deg); 
+            } 
+        }
+    </style>
+    <img id="img360" src="../file/icons/icon-white.png">
     <h1>نعتذر يتم تجهيز هذه الصفحة الان</h1>
     <h2>يمكنك الرجوع فيما بعد او العودة للصفحة الرئيسية
-        <a href="<?=$indexP?>">الصفحة الرئيسية</a>
+        <a href="<?=$indexP?>" class="link1">الصفحة الرئيسية</a>
     </h2>
-
-    <?php
-    include $footer
-    ?>
 </body>
 </html>
