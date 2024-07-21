56a908822dbc52d2ea01ef023535ca9a370d1c35
ElBaron23 <imad.touzouz1@gmail.com>
2024-07-21 15:28:11 +0200

    delete bakup folder

--- backup/js/toggle_btn_nav.js
+++
@@ -1,6 +0,0 @@
-let toggle_btn =document.getElementById('toggle_btn'),
-    nav        =document.getElementById('nav');
-toggle_btn.addEventListener('click',()=>{
-toggle_btn.classList.toggle('toggle');
-nav.classList.toggle('show_nav');
-});
