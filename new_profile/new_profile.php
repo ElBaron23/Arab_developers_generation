7d609b63e7291438503f288cd44bb385e70cd367
Mouad Bourquouquou <m.bourquouquou1181@uca.ac.ma>
2024-07-27 04:55:12 -0700

    add_new_profil_need a revision

---
+++ new_profile/new_profile.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>profile</title>
+    <link rel="stylesheet" href="new_profile.css">
+</head>
+<body>
+    <div class="page">
+        <!-- الاسم و الصورة الشخصية -->
+        <header>
+            <figure><img src="icons_profile/image_profile.png" alt="can't have the image"></figure>
+            <h1>Mouad Bourquouquou</h1>
+        </header>
+        <!-- الايميل -->
+        <div class="mail">
+            <p>Email : <a href="#">mouad@gmail.com</a></p>
+        </div>
+        <!-- ايقونات اللغات القابلة للضغط -->
+        <section class="languages">
+            <div class="html">html</div>
+            <div class="js">js</div>
+            <div class="css">css</div>
+        </section>
+        <!-- ازرار التعديل على البروفايل و الحساب -->
+        <section class="modification">
+            <button class="modi_profile">تعديل الملف الشخصي</button>
+            <button class="modi_account">تعديل الحساب</button>
+        </section>
+        <!-- التواصل -->
+        <section class="contact">
+            <h3 class=p_contact > تواصل معي :</h3>
+            <figure class="fig_contact">
+                <a href="#"><img src= "icons_profile/icon_face.png" alt="facebook"></a>
+                <a href="#"> <img src= "icons_profile/icon_instagram.png" alt="instagrame"></a>
+                <a href="#"><img src= "icons_profile/icon_X(tweet).png" alt="X"></a>
+                <a href="#"><img src= "icons_profile/icon_linkedin.png" alt="LinkedIn"></a>  
+            </figure>
+        </section>
+        <!-- ملخص السيرة الذاتية -->
+        <section class=details>
+            <h3> معلومات اكثر :</h3>
+            <p>معاذ بوركوكو طالب هندسة معلوميات ،مهتم بتصميم و انشاء المواقع الالكترونية ،
+                خبرة متوسطة مع قابلية كبيرة للتعلم و التطور</p>
+        </section>
+        <!-- المتابعين و المشاريع -->
+        <div class="folowers_and_projects">
+        <section class="folowers">
+            <img src="icons_profile/icon_folowers.png" alt="icon_folowers">
+            <p class="p_fol">المتابعين : 3</p>
+        </section>
+        <section class="projects">
+            <img src="icons_profile/icon_projects.png" alt="icon_projects">
+            <p class="p_projects">المشاريع : 5</p>
+        </section>
+        </div>
+        <hr>
+    </div>
+</body>
+</html>
+<!-- انتهى -->
